5bd1f5fc47252de0b801feb68ddc5a3f8e3957f1
nabilnajmiii <108652331+nabilnajmiii@users.noreply.github.com>
2023-09-19 11:56:03 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <style>
+body {
+    font-family: Arial, sans-serif;
+}
+
+.login-container {
+    display: flex;  
+}
+
+form {
+    margin-left:900px;
+    text-align: left;
+}
+
+label {
+    display: block;
+    margin-bottom: 10px;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+button {
+    background-color: #000; /* Warna hitam */
+    color: #fff; /* Warna putih */
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+
+ </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+   
+</head>
+<body>
+    <div class="image">
+            <img src="1.jpg" width="40%" >
+        </div>
+    <div class="login-container">
+        
+        <div class="form">
+            <form>  
+
+            <div id="id01" class="modal">
+            
+             <div class="container">
+                <h1>Sign Up</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr>
+                <b>Email</b>
+                <input type="text" placeholder="Enter Email" name="email" >
+
+                <b>Password</b>
+                <input type="password" placeholder="Enter Password" name="psw" >
+
+               <b>Repeat Password</b>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" >
+      
+                <button type="submit" class="signupbtn">Sign Up</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+
+            </form>
+
+        </div>
+    
+
+</body>
+</html>
